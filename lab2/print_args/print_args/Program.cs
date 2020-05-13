db9933f16e0e0648fce7d221529159437a37a50c
u6ele66 <56578936+u6ele66@users.noreply.github.com>
2020-05-13 14:30:59 +0300

    Add files via upload

---
+++ lab2/print_args/print_args/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace print_args
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Number of arguments: " + args.Length);
+                Console.Write("Arguments:");
+
+                for (ushort i = 0; i < args.Length; i++)
+                {
+                    Console.Write(" " + args[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No parameters were specified!");
+            }
+        }
+    }
+}
---
+++ lab2/remove_duplicates/remove_duplicates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!");
+                Console.WriteLine("Usage remove_duplicates.exe <input string>");
+            }
+
+            else
+            {
+                string str = args[0];
+                Console.WriteLine(str.Distinct().ToArray());
+            }
+        }
+    }
+}
